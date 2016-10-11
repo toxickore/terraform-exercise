3aaec2b4f2b666a542af0140000b22e6fc881c51
emerson <emerson@epplkraw0166t1.princeton.epam.com>
2016-10-11 16:43:45 -0500

    first subnet added to the exercise terraform script

--- exercise.tf
+++ exercise.tf
@@ -1,11 +1,20 @@
-resource "aws_vpc" "example" {
+resource "aws_vpc" "example_vpc" {
 	cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "gw" {
-	vpc_id = "${aws_vpc.example.id}"
+resource "aws_internet_gateway" "example_igw" {
+	vpc_id = "${aws_vpc.example_vpc.id}"
 
 	tags {
-		Name = "main"
+		Name = "example_igw"
+	}
+}
+
+resource "aws_subnet" "example_priv01_subnet" {
+	vpc_id = "${aws_vpc.example_vpc.id}"
+	cidr_block = "10.0.1.0/24"
+
+	tags {
+		Name = "example_priv01_subnet"
 	}
 }
