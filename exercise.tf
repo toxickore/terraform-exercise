afbd5e81f42eb227ff5f8f59a15b83d65ebfb894
emerson <emerson@epplkraw0166t1.princeton.epam.com>
2016-10-11 16:31:45 -0500

    create VPC and internet gateway

--- exercise.tf
+++ exercise.tf
@@ -1,5 +1,11 @@
 resource "aws_vpc" "example" {
-	cidr_block	= "10.0.0.0/16"
+	cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "gw" {
+	vpc_id = "${aws_vpc.example.id}"
 
+	tags {
+		Name = "main"
+	}
+}
