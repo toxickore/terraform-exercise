acc1059dace3b3398d3f4b38239694082cb89d0c
emerson <emerson@epplkraw0166t1.princeton.epam.com>
2016-10-11 20:48:04 -0500

    adding route table and additional subnets

--- exercise.tf
+++ exercise.tf
@@ -17,6 +17,7 @@ resource "aws_subnet" "example_priv01_subnet" {
 	tags {
 		Name = "example_priv01_subnet"
 	}
+}
 
 resource "aws_subnet" "example_priv02_subnet" {
 	vpc_id = "${aws_vpc.example_vpc.id}"
@@ -24,5 +25,35 @@ resource "aws_subnet" "example_priv02_subnet" {
 
 	tags {
 		Name = "example_priv02_subnet"
+	}
+}
+
+resource "aws_subnet" "example_pub01_subnet" {
+	vpc_id = "${aws_vpc.example_vpc.id}"
+	cidr_block = "10.0.3.0/24"
+
+	tags {
+		Name = "example_pub01_subnet"
+	}
+}
+
+resource "aws_subnet" "example_pub02_subnet" {
+	vpc_id = "${aws_vpc.example_vpc.id}"
+	cidr_block = "10.0.4.0/24"
+
+	tags {
+		Name = "example_pub02_subnet"
+	}
+}
+
+resource "aws_route_table" "example_pub_rt" {
+	vpc_id = "${aws_vpc.example_vpc.id}"
+	route {
+		cidr_block = "10.0.1.0//24"
+		gateway_id = "${aws_internet_gateway.example_igw.id}"
+	}
+	
+	tags {
+		Name = "example_pub_rt"
 	}
 }
